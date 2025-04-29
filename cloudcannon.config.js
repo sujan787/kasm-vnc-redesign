5fcd30d03c2e6b4db4d9b98e5e7d1050becf8b92
sujan787 <sujanmoi787@gmail.com>
2025-04-29 17:22:19 +0530

    added new changes

---
+++ cloudcannon.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  collections_config: {
+    page: {
+      name: 'page',
+      path: 'app',
+      parser: 'frontmatter',
+      output: true,
+      url: '/',
+      editor: {
+        default: 'visual' 
+      }
+    },
+   
+  }
+}
