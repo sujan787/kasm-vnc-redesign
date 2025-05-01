b00ee0c8519d3e133b601928982924b13a279c09
sujan787 <sujanmoi787@gmail.com>
2025-05-01 14:33:47 +0530

    added new changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,14 @@ import NavBar from "../components/nav-bar"
 import Footer from "../components/footer"
 
 // @ts-ignore
-// import Filer from '@cloudcannon/filer';
+import Filer from '@cloudcannon/filer';
 import Blocks from "@/components/blocks";
-import json from "@/content/pages/index.json"
 
 export default async function Home() {
 
-  // const filer = new Filer({ path: 'content' });
-  // const json = await filer.getItem('index.md', { folder: 'pages' });
-  // const page = JSON.parse(JSON.stringify(json))
-  const page = { data: json }
+  const filer = new Filer({ path: 'content' });
+  const json = await filer.getItem('index.md', { folder: 'pages' });
+  const page = JSON.parse(JSON.stringify(json))
 
   return (
     <div className="min-h-screen bg-gradient-to-b from-sky-50 to-white ">
